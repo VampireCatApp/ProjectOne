915fd50f35179b755c6f33763176d5636503e206
Marcin Mazur Laptop <marcin3mazur@gmail.com>
2017-09-14 13:18:41 +0200

    add line

--- src/com/vampirecat/Main.java
+++ src/com/vampirecat/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Number one" + 1);
+        System.out.println("Number two" + 2);
     }
 }
