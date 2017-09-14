e03aefe30b8006f68d2ef549e7f6252491160458
Marcin Mazur <marcin3mazur@gmail.com>
2017-09-14 13:22:27 +0200

    add line 3

--- src/com/vampirecat/Main.java
+++ src/com/vampirecat/Main.java
@@ -1,9 +1,12 @@
 package com.vampirecat;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("Number one" + 1);
         System.out.println("Number two" + 2);
+        System.out.println("Number three" + 3 );
     }
 }
